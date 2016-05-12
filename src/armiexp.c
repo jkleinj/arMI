855f1cc5bf24de9a9e540e97ad0909a140b73c80
jkleinj <jens@jkleinj.eu>
2016-05-12 13:26:33 +0100

    correction

--- src/armiexp.c
+++ src/armiexp.c
@@ -323,14 +323,11 @@ int main(int argc, char *argv[])
 	long double completion = 0.;
 	unsigned int completion_i = 0;
 
-<<<<<<< HEAD
 	/* probability check */
 	double p_sum = 0.;
-=======
 	/* scale factor to map expression values to 20 bins */
-	float scafa = 0.;
-	double l2min = 0.; /* log2 of readmin */
->>>>>>> 489e4697ed1840211110387031fde492b9ee9031
+	/* float scafa = 0.;*/
+	/*double l2min = 0.;*/ /* log2 of readmin */
 
 	/*____________________________________________________________________________*/
 	/* random seed */
@@ -383,22 +380,20 @@ int main(int argc, char *argv[])
 	gsl_matrix *level = gsl_matrix_calloc(N, L); /* expression level matrix */
 
 	expr.maxLevel = 1;
-	scafa = 19. / (log2(expr.readmax) - log2(expr.readmin));
+	/*scafa = 19. / (log2(expr.readmax) - log2(expr.readmin));
 	l2min = log2(expr.readmin);
 #ifdef DEBUG
 	fprintf(stderr, "readmin %f,  readmax %f, scafa %f\n",
 			expr.readmin, expr.readmax, scafa);
 #endif
+	*/
 	for (n = 0; n < expr.nrow; ++ n) {
 		for (l = 0; l < expr.ncol; ++ l) {
 			assert((expr.read[n][l] >= 0.) && "expression values should be positive");
 			if (expr.read[n][l] > 0.) {
-<<<<<<< HEAD
 				/* to simplify things, reading expression levels */
 				gsl_matrix_set(level, n, l, (double)expr.read[n][l]);
-=======
-				gsl_matrix_set(level, n, l, (double)(roundf((log2(expr.read[n][l]) - l2min) * scafa) + 1.));
->>>>>>> 489e4697ed1840211110387031fde492b9ee9031
+				/*gsl_matrix_set(level, n, l, (double)(roundf((log2(expr.read[n][l]) - l2min) * scafa) + 1.));*/
 				expr.maxLevel = max((int)gsl_matrix_get(level, n, l), expr.maxLevel);
 			} else {
 				gsl_matrix_set(level, n, l, (double)1.);
